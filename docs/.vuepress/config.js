027b4f825188e0e37f565c1ebc19d7206b96d65f
whistle <435424527@qq.com>
2025-02-17 12:45:57 +0800

    202502171245

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,13 +27,34 @@ export default defineUserConfig({
         text: "准备工作",
         link: "/getting-started/快速索引.md",
         children: [
-          {text: "快速索引", link: "/getting-started/快速索引.md"}
+          {text: "快速索引", link: "/getting-started/快速索引.md"},
+          {text: "编程软件", link: "/getting-started/编程软件.md"},
+          {text: "材料清单", link: "/getting-started/材料清单.md"},
+          {text: "结构搭建", link: "/getting-started/结构搭建.md"},
+          {text: "注意事项", link: "/getting-started/注意事项.md"},
+          {text: "服务支持", link: "/getting-started/服务支持.md"}
         ]
       },
       {
         text: "避障机器人",
         link: "/basic/避障机器人.md",
-        collapsible: true,
+        // collapsible: true, /* 含子目录时取消注释 */
+      },
+      {
+        text: "沿墙行走机器人",
+        link: "/basic/沿墙行走机器人.md",
+      },
+      {
+        text: "巡线机器人",
+        link: "/basic/巡线机器人.md",
+      },
+      {
+        text: "光强检测机器人",
+        link: "/basic/巡线机器人.md",
+      },
+      {
+        text: "跟随机器人",
+        link: "/basic/巡线机器人.md",
       },
     ],
     sidebarDepth: 0
